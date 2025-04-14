01092740d0742347ffb9b3ff84599b2f3dcabcb7
Melissa <57367085+melpicazo@users.noreply.github.com>
2025-04-13 19:24:21 -0600

    chore: connect to DB via `DATABASE_URL`

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
 import "dotenv/config";
-// import pool from "./db/db";
 import uploadRoutes from "./routes/upload.route";
+import pool from "db/db";
 
 function logInitializationStep(message: string) {
   const GREEN_COLOR_FORMATTING = "\x1b[32m%s\x1b[0m";
@@ -11,20 +11,16 @@ function logInitializationStep(message: string) {
 
 const app = express();
 
-// // Check if the database is running
-// pool
-//   .query("SELECT 1")
-//   .then(() => {
-//     logInitializationStep(
-//       `Connected to PostgreSQL instance running on port ${
-//         process.env.DB_PORT || "5432"
-//       }`
-//     );
-//   })
-//   .catch((err) => {
-//     console.error("Unable to connect to the database:", err);
-//     process.exit(1);
-//   });
+/* Check if the database is running */
+pool
+  .query("SELECT 1")
+  .then(() => {
+    logInitializationStep("Connected to PostgreSQL instance");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+    process.exit(1);
+  });
 
 app.use(cors());
 app.use(express.json());
@@ -53,7 +49,7 @@ app.use("/api/upload", uploadRoutes);
 if (process.env.NODE_ENV !== "production") {
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    logInitializationStep(`Server running at http://localhost:${port}`);
   });
 }
 
