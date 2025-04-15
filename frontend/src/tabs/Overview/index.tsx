dde3f218246985178567d64037600efb4154d032
Melissa <57367085+melpicazo@users.noreply.github.com>
2025-04-15 00:54:55 -0600

    style: align key metric card values to bottom of card

--- frontend/src/tabs/Overview/index.tsx
+++ frontend/src/tabs/Overview/index.tsx
@@ -29,12 +29,12 @@ export const Overview = () => {
                   if (!metric.value) return;
                   return (
                     <Card key={metric.id}>
-                      <div className="flex flex-col gap-4">
+                      <div className="flex flex-col gap-4 h-full">
                         <h4 className="font-semibold flex gap-2 items-center text-sky-700">
                           <metric.Icon className="w-5 h-5 shrink-0 stroke-2" />{" "}
                           {metric.title}
                         </h4>
-                        <div className="flex flex-col gap-2">
+                        <div className="flex flex-col flex-1 justify-between gap-2">
                           <p className="text-sm">{metric.description}</p>
                           <p className="text-xl font-heading tracking-wider text-emerald-600">
                             {metric.value}
